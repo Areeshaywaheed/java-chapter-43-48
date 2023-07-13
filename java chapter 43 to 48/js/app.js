55a8385e0af638d280c3459463a51f6313e861b4
Areeshaywaheed <128454186+Areeshaywaheed@users.noreply.github.com>
2023-07-13 19:39:34 +0500

    Add files via upload

---
+++ java chapter 43 to 48/js/app.js
@@ -0,0 +1,43 @@
+            //  ============ chapter 38-42 started=============
+
+    //    ========question number 1=========
+
+// 1. Show an alert box on click on a link.
+
+// function myAlert() {
+//     alert("Have a Nice Journey");
+// }
+
+// =========question number 2 is present in index.html file=====
+
+    // ==========question number 3============
+
+// function deletedRow(btn) {
+//     var row = btn.parentNode.parentNode;
+//     console.log(row);
+//     var rowIndex = row.rowIndex;
+//     console.log(rowIndex);
+//     row.parentNode.removeChild(row);
+
+//     var table = document.getElementsByTagName("table")[0];
+//     var rowCount = table.rows.length;
+
+//     for (var i = rowIndex; i < rowCount; i++) {
+//         var row = table.rows[i];
+//         row.cells[0].textContent = i - 1;
+//     }
+// }
+
+//    =========question number 4 is present in index.html file======
+
+
+    //   ============question number 5==========
+
+// function counter1() {
+//     document.getElementById("count").innerHTML++; 
+// }
+// function counter2() {
+//     document.getElementById("count").innerHTML--; 
+// }
+
+          //  ============ chapter 38-42 completed=============
